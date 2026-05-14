b6b3928880df578f6b2146c7cd830934599ced86
Lyna <git@blooym.dev>
2026-05-14 09:12:26 +0100

    refactor: add features to mediautil, no defaults

--- crates/mediautil/src/metadata/mod.rs
+++ crates/mediautil/src/metadata/mod.rs
@@ -1,5 +1,9 @@
+#[cfg(feature = "image-metadata")]
 mod image;
+#[cfg(feature = "video-metadata")]
 mod video;
 
+#[cfg(feature = "image-metadata")]
 pub use image::*;
+#[cfg(feature = "video-metadata")]
 pub use video::*;
