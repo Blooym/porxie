06a72ec38025ce24cf8cc52d593c587dc44045a3
Lyna <git@blooym.dev>
2026-08-21 16:51:16 +0100

    flake: upgrade jacquard flake to 0.12.1

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
       url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
     jacquard = {
-      url = "git+https://tangled.org/nonbinary.computer/jacquard?rev=dd2e2bbf6bcbfd5e9cf1727bddb828a3f0038802"; # 0.11
+      url = "git+https://tangled.org/nonbinary.computer/jacquard?rev=714a5da87d012aca82f48522ea441ca2a49752c2"; # 0.12.1
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -38,7 +38,7 @@
 
               # Nix
               nil
-              nixsd
+              nixd
             ];
             env = {
               RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
